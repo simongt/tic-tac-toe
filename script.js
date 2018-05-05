e017cfff0437d83ec99e17fb549cf7945b34c2fe
Simon G Tsegay <simon@ool-ad0203a1.dyn.optonline.net>
2018-05-05 19:00:28 -0400

    I implemented Akseli Palén's solution for calculating combinations of elements in Array

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ $(function() {
       wins: scoreBoard.oWins // can just init with 0
     }   
   };
+  // list of winning combos
   let winningCombos = [
     [0,1,2],
     [0,3,6],
@@ -122,5 +123,64 @@ $(function() {
       }
     }
     return false;
+  }
+
+  // Akseli Palén's solution for calculating combinations of elements in Array
+  // Github: https://gist.github.com/axelpale/3118596
+  //
+  // Algorithm description:
+  // To get k-combinations of a set, we want to join each element
+  // with all (k-1)-combinations of the other elements. The set of
+  // these k-sized sets would be the desired result. However, as we
+  // represent sets with lists, we need to take duplicates into
+  // account. To avoid producing duplicates and also unnecessary
+  // computing, we use the following approach: each element i
+  // divides the list into three: the preceding elements, the
+  // current element i, and the subsequent elements. For the first
+  // element, the list of preceding elements is empty. For element i,
+  // we compute the (k-1)-computations of the subsequent elements,
+  // join each with the element i, and store the joined to the set of
+  // computed k-combinations. We do not need to take the preceding
+  // elements into account, because they have already been the i:th
+  // element so they are already computed and stored. When the length
+  // of the subsequent list drops below (k-1), we cannot find any
+  // (k-1)-combs, hence the upper limit for the iteration.
+  function getCombosOf(set, k) {
+    var i, j, combos, head, tailCombos;
+
+    // There is no way to take e.g. sets of 5 elements from
+    // a set of 4.
+    if (k > set.length || k <= 0) {
+      return [];
+    }
+
+    // K-sized set has only one K-sized subset.
+    if (k == set.length) {
+      return [set];
+    }
+
+    // There is N 1-sized subsets in a N-sized set.
+    if (k == 1) {
+      combos = [];
+      for (i = 0; i < set.length; i++) {
+        combos.push([set[i]]);
+      }
+      return combos;
+    }
+
+    // Assert {1 < k < set.length}
+    combos = [];
+    for (i = 0; i < set.length - k + 1; i++) {
+      // head is a list that includes only our current element.
+      head = set.slice(i, i + 1);
+      // We take smaller combinations from the subsequent elements
+      tailCombos = getCombosOf(set.slice(i + 1), k - 1);
+      // For each (k-1)-combination we join it with the current
+      // and store it to the set of k-combinations.
+      for (j = 0; j < tailCombos.length; j++) {
+        combos.push(head.concat(tailCombos[j]));
+      }
+    }
+    return combos;
   }
 });
