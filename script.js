ba438761af6c3718db12f65800aa32f3b460fed9
Simon G Tsegay <simon@ool-ad0203a1.dyn.optonline.net>
2018-05-05 13:04:42 -0400

    depending on player turn, box click will x or o

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 // $(document).ready(function () {
 $(function() {
+  let countTurns = 0;
   // grab the html body
   const $body = $('body');
-  // create a table that will act as a gameboard
+  // create a grid that will act as a gameboard
   const $board = $('<div>');
   $board.addClass('board');
   $board.appendTo($body);
-  // within this table, there'll be 9 boxes (3x3)
+  // within this grid, there'll be 9 boxes (3x3)
   const numBoxes = 9;
   for (let i = 0; i < numBoxes; i++) {
     let $box = createBox();
     let boxClick = $box.click(function() {
+      countTurns++;
       console.log(`Box ${i} clicked.`);
+      if(countTurns % 2) {
+        $box.text('x');
+      } else {
+        $box.text('o');
+      }
+      $box.off();
     });
     $box.appendTo($board);
   }
